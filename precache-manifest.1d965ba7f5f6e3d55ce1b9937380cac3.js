60fd677405051744b51bc45f970814ad8ee89775
cyberkiller7 <51155142+cyberkiller7@users.noreply.github.com>
2020-05-28 23:07:45 +0530

    Updates

---
+++ precache-manifest.1d965ba7f5f6e3d55ce1b9937380cac3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd859e6183e4b55a23cb251773b5b1c1",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "a4f88bb9d90d5cba8f0f",
+    "url": "/jammming/static/css/main.7b9ab371.chunk.css"
+  },
+  {
+    "revision": "d173f3af893ce4eb1ef2",
+    "url": "/jammming/static/js/2.051a2115.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.051a2115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f88bb9d90d5cba8f0f",
+    "url": "/jammming/static/js/main.a9179e4a.chunk.js"
+  },
+  {
+    "revision": "6ee62a957b7814eb82f7",
+    "url": "/jammming/static/js/runtime-main.914a1974.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
